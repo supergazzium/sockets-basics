feb36f7e61a666fa57ee4a15636a1a9648370cbe
Prachum Chanman <gazzium@gmail.com>
2016-11-03 13:10:38 -0400

    two browser can talk to each other

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,8 @@ socket.on('connect',function(){
     console.log('Connected to socket.io server!');
 
 });
+
+socket.on('message', function(message){
+   console.log('New message: ');
+   console.log(message.text);
+});
